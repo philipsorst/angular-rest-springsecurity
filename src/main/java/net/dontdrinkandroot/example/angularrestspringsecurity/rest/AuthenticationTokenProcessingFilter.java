cd349c724084e54f96dceb9c1efd9ef70e2f697a
Philip Washington Sorst <philip@sorst.net>
2014-03-07 12:10:05 +0100

    Cleaning up AuthenticationTokenProcessingFilter

--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/rest/AuthenticationTokenProcessingFilter.java
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/rest/AuthenticationTokenProcessingFilter.java
@@ -31,21 +31,20 @@ public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
 			ServletException {
 
-		if (!(request instanceof HttpServletRequest)) {
-			throw new RuntimeException("Expecting a HTTP request");
-		}
+		HttpServletRequest httpRequest = this.getAsHttpRequest(request);
 
-		HttpServletRequest httpRequest = (HttpServletRequest) request;
 		String authToken = this.extractAuthTokenFromRequest(httpRequest);
-
 		String userName = TokenUtils.getUserNameFromToken(authToken);
 
 		if (userName != null) {
+
 			UserDetails userDetails = this.userService.loadUserByUsername(userName);
+
 			if (TokenUtils.validateToken(authToken, userDetails)) {
+
 				UsernamePasswordAuthenticationToken authentication =
 						new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails((HttpServletRequest) request));
+				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(httpRequest));
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			}
 		}
@@ -54,9 +53,22 @@ public class AuthenticationTokenProcessingFilter extends GenericFilterBean {
 	}
 
 
+	private HttpServletRequest getAsHttpRequest(ServletRequest request) {
+
+		if (!(request instanceof HttpServletRequest)) {
+			throw new RuntimeException("Expecting an HTTP request");
+		}
+
+		return (HttpServletRequest) request;
+	}
+
+
 	private String extractAuthTokenFromRequest(HttpServletRequest httpRequest) {
 
+		/* Get token from header */
 		String authToken = httpRequest.getHeader("X-Auth-Token");
+
+		/* If token not found get it from request parameter */
 		if (authToken == null) {
 			authToken = httpRequest.getParameter("token");
 		}
