a46956403468814f7792c50b4ee6f129e2e5c7de
Philip Washington Sorst <philip@sorst.net>
2016-09-23 16:53:26 +0200

    Adding AccessToken entity

---
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/entity/AccessToken.java
@@ -0,0 +1,65 @@
+package net.dontdrinkandroot.example.angularrestspringsecurity.entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.util.Date;
+
+/**
+ * @author Philip Washington Sorst <philip@sorst.net>
+ */
+@javax.persistence.Entity
+public class AccessToken implements Entity
+{
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @ManyToOne
+    private User user;
+
+    @Column
+    private Date expiry;
+
+    protected AccessToken()
+    {
+        /* Reflection instantiation */
+    }
+
+    public AccessToken(User user, String token)
+    {
+        this.user = user;
+        this.token = token;
+    }
+
+    public AccessToken(User user, String token, Date expiry)
+    {
+        this(user, token);
+        this.expiry = expiry;
+    }
+
+    @Override
+    public Long getId()
+    {
+        return this.id;
+    }
+
+    public String getToken()
+    {
+        return this.token;
+    }
+
+    public User getUser()
+    {
+        return this.user;
+    }
+
+    public Date getExpiry()
+    {
+        return this.expiry;
+    }
+}
--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/entity/Entity.java
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/entity/Entity.java
@@ -4,4 +4,5 @@ import java.io.Serializable;
 
 public interface Entity extends Serializable
 {
+    Long getId();
 }
