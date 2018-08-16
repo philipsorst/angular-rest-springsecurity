84c39cb6839c31f1ea15918b853b9f0baa88643e
Philip Washington Sorst <philip@sorst.net>
2018-08-16 18:28:24 +0200

    Adding angular

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+@NgModule({
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule
+{
+}
