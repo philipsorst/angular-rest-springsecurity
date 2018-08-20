9349098e821c4aad346c7c09d047b9e5be925628
Philip Washington Sorst <philip@sorst.net>
2018-08-20 23:48:04 +0200

    Subresource pagination and exclusion

--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/repository/BlogPostRepository.java
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/repository/BlogPostRepository.java
@@ -2,16 +2,21 @@ package net.dontdrinkandroot.example.angularrestspringsecurity.repository;
 
 import net.dontdrinkandroot.example.angularrestspringsecurity.entity.BlogPost;
 import net.dontdrinkandroot.example.angularrestspringsecurity.entity.BlogPostListProjection;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestResource(path = "blog-posts", excerptProjection = BlogPostListProjection.class)
 public interface BlogPostRepository extends PagingAndSortingRepository<BlogPost, Long>
 {
+    @RestResource(exported = false)
     Optional<BlogPost> findBySlug(String slug);
 
-    List<BlogPost> findByAuthorUsername(String username);
+    @RestResource(path = "author", rel = "author")
+    Page<BlogPost> findByAuthorUsername(@Param("name") String username, Pageable pageable);
 }
